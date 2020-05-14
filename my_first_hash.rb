dce6f29a6e90627abcec95cf62f487d941d21e24
Hannah Sandusky <hnnahsandusky@gmail.com>
2020-05-14 01:40:28 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-my_hash => {"Macbook Pro", "iPhone", "iPad", "iMac"}
+my_hash => {"Macbook Pro", "iPhone", "iPad", "iMac", "Airpods"}
 
 end
 
